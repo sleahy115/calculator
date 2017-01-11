2e232f1046d0f49b1ecd24a24f6111fc63e2b16a
sarah leahy <sarah leahy>
2017-01-11 16:28:30 -0700

    add forms for addition, subtraction, multiply and divide'

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,43 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result =add(number1, number2);
+  $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#sub1").val());
+  var number2 = parseInt($("#sub2").val());
+  var result =subtract(number1, number2);
+  $("#output").text(result);
+  });
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#multiply1").val());
+  var number2 = parseInt($("#multiply2").val());
+  var result =multiply(number1, number2);
+  $("#output").text(result);
+  });
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+  var number1 = parseInt($("#divide1").val());
+  var number2 = parseInt($("#divide2").val());
+  var result =divide(number1, number2);
+  $("#output").text(result);
+  });
+});
